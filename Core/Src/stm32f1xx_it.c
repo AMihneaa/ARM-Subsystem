d0ebde76bc1a1c81e70fbf854a6fd892af42289d
AMihneaa <asmihnea5@gmail.com>
2025-03-30 14:04:33 +0300

    Changed TIM2 interrupt

--- Core/Src/stm32f1xx_it.c
+++ Core/Src/stm32f1xx_it.c
@@ -228,7 +228,7 @@ void TIM2_IRQHandler(void)
 		TIM2_CallBack();
 	}
   /* USER CODE END TIM2_IRQn 0 */
-  HAL_TIM_IRQHandler(&htim2);
+//  HAL_TIM_IRQHandler(&htim2);
   /* USER CODE BEGIN TIM2_IRQn 1 */
 
   /* USER CODE END TIM2_IRQn 1 */
